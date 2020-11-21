54e7756a7e37912bf8946378e48100d1b30db30b
rivafarabi <rivafarabi.babeheer@gmail.com>
2020-11-22 02:43:36 +0700

    init file

---
+++ index.js
@@ -0,0 +1,60 @@
+const moment = require('./moment');
+
+class DeckboardClock {
+	constructor(props) {
+		this.setValue = props.setValue;
+		this.name = 'Clock';
+		this.platforms = ['WINDOWS', 'LINUX'];
+
+		this.inputs = [
+			{
+				label: 'Display Time',
+				value: 'clock-display-time',
+				icon: 'clock',
+				mode: 'custom-value',
+				fontIcon: 'fas',
+				color: '#1a1a1a',
+				input: [
+					{
+						label: 'Select Clock Format',
+						type: 'input:select',
+						items: [
+							{ value: 'clock-12h', label: '12 Hour Format' },
+							{ value: 'clock-24h', label: '24 Hour Format' }
+						]
+					}
+				]
+			}
+		];
+		this.configs = [];
+		this.isFirstInit = true;
+	}
+
+	get selections() {
+		return [{
+			header: this.name
+		}, ...this.inputs];
+	}
+
+	// Executes when the extensions loaded every time the app start.
+	initExtension() {
+		this.updateClock();
+		setInterval(() => {
+			if (this.isFirstInit) this.isFirstInit = false;
+			this.updateClock()
+		}, this.isFirstInit ? 60000 - (moment().get('seconds') * 1000) : 60000);
+	}
+
+	updateClock() {
+		this.setValue({
+			'clock-12h': moment().format('H:mm A'),
+			'clock-24h': moment().format('h:mm')
+		})
+	}
+
+	execute() {
+		return;
+	};
+}
+
+module.exports = (sendData) => new DeckboardClock(sendData);
